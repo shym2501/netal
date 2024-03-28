a498300980f4b7c572e49682d2f3eb69c778f4ca
Shym2501 <sulthannurya@gmail.com>
2024-03-28 13:33:15 +0800

    Initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: squooshImageService()
+  },
+  integrations: [tailwind()]
+});
