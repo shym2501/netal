0e8b8c10aaa12a820ce44cb792d3eb3cf7768026
Shym2501 <sulthannurya@gmail.com>
2024-03-28 15:31:00 +0800

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-	'node_modules/preline/dist/*.js',
+	'./node_modules/preline/dist/*.js',
 	],
 	theme: {
 		extend: {},
